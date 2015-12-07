c6cd0eaf1c5b90783504f62860bb2fafb3f08584
Alexander Brauckmann <a.brauckmann@gmail.com>
2015-12-06 18:27:32 -0800

    RemotePlayer implemented.

--- RemotePlayer.cpp
+++ RemotePlayer.cpp
@@ -1,7 +1,40 @@
 #include "RemotePlayer.h"
 
 //-------------------------------------------------------------------------
-RemotePlayer::RemotePlayer(std::string name) 
-  : Player(name) {
+RemotePlayer::RemotePlayer(std::string playerName,
+                           int heigth,
+                           int width,
+                           int serverPort)
+  : Player(playerName) {
     
+  _receiver.reset(new NetworkReceiver(width, heigth, this));
+  _receiver->Listen(serverPort);
+}
+
+//-------------------------------------------------------------------------
+cv::Mat RemotePlayer::getImage() {
+  
+  return _currentImg;
+}
+
+//-------------------------------------------------------------------------
+FaceData RemotePlayer::getFaceData() {
+  
+  return _faceData;
+}
+
+//-------------------------------------------------------------------------
+void RemotePlayer::OnReceived(cv::Mat mat, Message msg) {
+
+  mat.copyTo(_currentImg);
+
+  // Parse FaceData
+  FaceData fd;
+  fd.center = msg.center;
+  fd.topleft = msg.topleft;
+  fd.topright = msg.topright;
+  fd.downright = msg.downright;
+  fd.downleft = msg.downright;
+
+  _faceData = fd;
 }
--- RemotePlayer.h
+++ RemotePlayer.h
@@ -2,26 +2,38 @@
 #define REMOTEPLAYER_H_
 
 #include "player.h"
+#include "network/NetworkReceiver.h"
+#include "network/IReceiverCallback.h"
 
 #include <memory>
 
 
 /*
- *
+ * A class representing a remote player. It gets the
+ * remote data by opening a TCP server.
  */
 class RemotePlayer
-  : public Player {
+  : public Player,
+    public IReceiverCallback {
     
 public:
-  RemotePlayer(std::string name);
+  RemotePlayer(std::string playerName,
+               int heigth,
+               int width,
+               int serverPort);
   virtual ~RemotePlayer() {}
 
 // From Player
 public:
-  virtual cv::Mat getImage() {}
-  virtual FaceData getFaceData() {}
+  virtual cv::Mat getImage();
+  virtual FaceData getFaceData();
+
+//IReceiverCallback
+public:
+  virtual void OnReceived(cv::Mat mat, Message msg);
 
 private:
+  NetworkReceiverPtr _receiver;
 
 };
 typedef std::shared_ptr<RemotePlayer> RemotePlayerPtr;
